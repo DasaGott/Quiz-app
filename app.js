fc8719116b0028cb52823ba52bb6a4065c0119b6
DasaGott <142871686+DasaGott@users.noreply.github.com>
2024-01-09 14:00:37 +0100

    Added array of questions

---
+++ app.js
@@ -0,0 +1,46 @@
+const quizData = [
+  {
+    question: "How does a FOR loop start?",
+    a: "for (i = 0; i <= 5; i++) ",
+    b: "for (i = 0; i <= 5)",
+    c: "for (i <= 5; i++)",
+    d: "for i = 1 to 5",
+    correct: a,
+  },
+  {
+    question:
+      "How do you round the number 7.25, to the nearest integer?",
+    a: "Math.round(7.25)  ",
+    b: "round(7.25)",
+    c: "Math.rnd(7.25)",
+    d: "rnd(7.25)",
+    correct: a,
+  },
+  {
+    question:
+      "How do you find the number with the highest value of x and y?",
+    a: "Math.ceil(x, y) ",
+    b: "Math.max(x, y)  ",
+    c: "top(x, y)",
+    d: "ceil(x, y)",
+    correct: b,
+  },
+  {
+    question:
+      "How to write an IF statement for executing some code if 'i' is NOT equal to 5?",
+    a: "if (i <> 5)",
+    b: "if i <> 5",
+    c: "if (i != 5)",
+    d: "if i =! 5 then",
+    correct: c,
+  },
+  {
+    question:
+      "How can you detect the client's browser name?",
+    a: "browser.name",
+    b: "client.navName",
+    c: "client.name)",
+    d: "navigator.appName",
+    correct: d,
+  },
+];
