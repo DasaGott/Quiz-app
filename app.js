2aed8218eb862ff6003b9970616632fe3835c105
DasaGott <142871686+DasaGott@users.noreply.github.com>
2024-01-09 16:49:29 +0100

    Added JS and few styling adjustments

--- app.js
+++ app.js
@@ -5,7 +5,7 @@ const quizData = [
     b: "for (i = 0; i <= 5)",
     c: "for (i <= 5; i++)",
     d: "for i = 1 to 5",
-    correct: a,
+    correct: "a",
   },
   {
     question:
@@ -14,7 +14,7 @@ const quizData = [
     b: "round(7.25)",
     c: "Math.rnd(7.25)",
     d: "rnd(7.25)",
-    correct: a,
+    correct: "a",
   },
   {
     question:
@@ -23,7 +23,7 @@ const quizData = [
     b: "Math.max(x, y)  ",
     c: "top(x, y)",
     d: "ceil(x, y)",
-    correct: b,
+    correct: "b",
   },
   {
     question:
@@ -32,15 +32,80 @@ const quizData = [
     b: "if i <> 5",
     c: "if (i != 5)",
     d: "if i =! 5 then",
-    correct: c,
+    correct: "c",
   },
   {
     question:
       "How can you detect the client's browser name?",
     a: "browser.name",
     b: "client.navName",
-    c: "client.name)",
+    c: "client.name",
     d: "navigator.appName",
-    correct: d,
+    correct: "d",
   },
 ];
+
+const quizEl = document.getElementById("quiz");
+const questionEl =
+  document.getElementById("question");
+const answerEls =
+  document.querySelectorAll(".answer");
+const aText = document.getElementById("a_text");
+const bText = document.getElementById("b_text");
+const cText = document.getElementById("c_text");
+const dText = document.getElementById("d_text");
+const submitBtn =
+  document.getElementById("submit");
+
+let currentQuiz = 0;
+let score = 0;
+
+loadQuiz();
+
+function loadQuiz() {
+  deselectAnswers();
+  const currentQuizData = quizData[currentQuiz];
+  questionEl.innerText = currentQuizData.question;
+  aText.innerText = currentQuizData.a;
+  bText.innerText = currentQuizData.b;
+  cText.innerText = currentQuizData.c;
+  dText.innerText = currentQuizData.d;
+}
+
+function deselectAnswers() {
+  answerEls.forEach((answerEl) => {
+    answerEl.checked = false;
+  });
+}
+
+submitBtn.addEventListener("click", () => {
+  const answer = getSelected();
+
+  if (answer) {
+    if (
+      answer === quizData[currentQuiz].correct
+    ) {
+      score++;
+    }
+    currentQuiz++;
+  }
+
+  if (currentQuiz < quizData.length) {
+    loadQuiz();
+  } else {
+    const percentage =
+      (score / quizData.length) * 100;
+    quizEl.innerHTML = `<h2>Your score is this quiz is ${percentage} %</h2>
+    <button onclick="location.reload()">Reload<button>`;
+  }
+});
+
+function getSelected() {
+  let answer;
+  answerEls.forEach((answerEl) => {
+    if (answerEl.checked) {
+      answer = answerEl.id;
+    }
+  });
+  return answer;
+}
